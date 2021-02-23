3e698edde76b6e82e3425b58870dc80387f2b830
Edward Dean <ejdean6@gmail.com>
2021-02-23 13:25:37 +0000

    Initial Commit

---
+++ Social_Network/Social_NetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Social_NetworkApp.swift
+//  Social_Network
+//
+//  Created by Edward Dean on 23/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct Social_NetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
